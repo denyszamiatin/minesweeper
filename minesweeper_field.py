006ffd3234edb2cc2d68e813ae2f46ac314d5d15
Nativeborn <nativeborncitizen@gmail.com>
2016-09-13 19:51:57 +0300

    Code review #2

--- minesweeper_field.py
+++ minesweeper_field.py
@@ -1,7 +1,9 @@
 from random import randint
+
 FIELD_SIZE = 10
+MINES_QUANTITY = 10
 EMPTY_CELL = 0
-MINE_INDICATOR = 'M'
+MINE = 'M'
 
 
 def get_playing_field():
@@ -14,69 +16,64 @@ def get_playing_field():
     ]
 
 
-def get_mined_field():
+def get_random_coord(field_size):
+    return randint(0, field_size - 1)
+
+
+def set_mines(field):
     """
     returns random mined field for game
     """
-    mined_field = []
-    for i, j in enumerate(get_playing_field()):
-        j.insert(randint(0, FIELD_SIZE), MINE_INDICATOR)
-        mined_field.append(j)
-    return mined_field
+    mines = 0
+    while mines < MINES_QUANTITY:
+        x = get_random_coord(FIELD_SIZE)
+        y = get_random_coord(FIELD_SIZE)
+        if field[x][y] != EMPTY_CELL:
+            continue
+        field[x][y] = MINE
+        mines += 1
+    return field
 
 
-def mine_calculation(array, x, y):
+def is_coords_in_range(x, y):
+    return 0 <= x < FIELD_SIZE and 0 <= y < FIELD_SIZE
+
+
+def is_mine(x, y):
+    return field[x][y] == MINE
+
+
+def mine_calculation(field, x, y):
     """
     returns the quantity of mines in neighboring cells
     """
-    mine_qty = 0
-
-    if array[x][y] == MINE_INDICATOR:
+    if field[x][y] == MINE:
         return 'M'
-    else:
-        for range_x in (-1, 0, 1):
-            for range_y in (-1, 0, 1):
-                if x + range_x >= 0 and y + range_y >= 0:
-                    try:
-                        cell_check = array[x + range_x][y + range_y]
-                    except IndexError:
-                        break
-                    else:
-                        if cell_check == MINE_INDICATOR:
-                            mine_qty += 1
-        return mine_qty
+
+    mines = 0
+    for range_x in (-1, 0, 1):
+        for range_y in (-1, 0, 1):
+            x_offset = x + range_x
+            y_offset = y + range_y
+            if is_coords_in_range(x_offset, y_offset) and \
+                    is_mine(x_offset, y_offset):
+                mines += 1
+    return mines
 
 
-playground = (get_mined_field())
+field = set_mines(get_playing_field())
+
 
 #show playground with mines
-for i in range(len(playground)):
-    for j in range(len(playground[i])):
-        print(playground[i][j], end=' ')
+for row in field:
+    for cell in row:
+        print(cell, end=' ')
     print()
 
 print()
 
 #show mines q-ty
-for i in range(len(playground)):
-    for j in range(len(playground[i])):
-        print(mine_calculation(playground, i, j), end=' ')
+for i in range(len(field)):
+    for j in range(len(field[i])):
+        print(mine_calculation(field, i, j), end=' ')
     print()
-    
-#function for input of cell coordinates    
-def input_coordinates():
-    while True:
-        try:
-            x = int(raw_input('Write number of line from 0 to %s:' %(FIELD_SIZE-1)))
-            y = int(raw_input('Write number of line from 0 to %s:' %(FIELD_SIZE-1)))
-            if x >= FIELD_SIZE or y >= FIELD_SIZE:
-                raise TypeError
-        except ValueError:
-            print ('Wrong input, try again')
-        except TypeError:
-            print ('Your number of coordinate is out of field')
-        else:
-            break
-    return x, y
-
-print (input_coordinates())
